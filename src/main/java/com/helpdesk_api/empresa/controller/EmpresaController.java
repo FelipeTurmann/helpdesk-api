d4a9878b326082d4b4a471792d37f4ba085aa41a
FelipeTurmann <felipe.turman@gmail.com>
2026-08-19 22:52:28 -0300

    feat: adiciona endpoint para cadastro de empresa

--- src/main/java/com/helpdesk_api/empresa/controller/EmpresaController.java
+++ src/main/java/com/helpdesk_api/empresa/controller/EmpresaController.java
@@ -1,6 +1,14 @@
 package com.helpdesk_api.empresa.controller;
 
+import com.helpdesk_api.empresa.dto.EmpresaRequestDto;
+import com.helpdesk_api.empresa.dto.EmpresaResponseDto;
+import com.helpdesk_api.empresa.service.EmpresaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmpresaController {
 
+    private final EmpresaService empresaService;
+
+    @PostMapping
+    public ResponseEntity<EmpresaResponseDto> criarEmpresa(@Valid @RequestBody EmpresaRequestDto requestDto) {
+
+        EmpresaResponseDto response = empresaService.criarEmpresa(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
 
 
 }
--- src/main/java/com/helpdesk_api/empresa/service/EmpresaService.java
+++ src/main/java/com/helpdesk_api/empresa/service/EmpresaService.java
@@ -1,9 +1,31 @@
 package com.helpdesk_api.empresa.service;
 
+import com.helpdesk_api.empresa.dto.EmpresaRequestDto;
+import com.helpdesk_api.empresa.dto.EmpresaResponseDto;
+import com.helpdesk_api.empresa.entity.EmpresaEntity;
+import com.helpdesk_api.empresa.mapper.EmpresaMapper;
+import com.helpdesk_api.exception.BusinessException;
+import com.helpdesk_api.repository.EmpresaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmpresaService {
+
+    private static EmpresaRepository empresaRepository;
+    private static EmpresaMapper empresaMapper;
+
+    public EmpresaResponseDto criarEmpresa(EmpresaRequestDto request) {
+
+        if (empresaRepository.existsByEmail(request.cnpj())) {
+            throw new BusinessException("Já existe uma empresa com esse cnpj cadastrada.");
+        }
+
+        EmpresaEntity empresaEntity = empresaMapper.toEntity(request);
+        empresaEntity.setAtivo(Boolean.TRUE);
+
+        EmpresaEntity empresaSalva = empresaRepository.save(empresaEntity);
+        return empresaMapper.toResponseDTO(empresaSalva);
+    }
 }
