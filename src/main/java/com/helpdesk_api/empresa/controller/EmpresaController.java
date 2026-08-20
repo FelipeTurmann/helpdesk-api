70a54567513968d96ffd74a31cea6355067ae3a2
FelipeTurmann <felipe.turman@gmail.com>
2026-08-20 20:55:43 -0300

    feat: adiciona preAuthorize a fluxo da empresa

--- src/main/java/com/helpdesk_api/empresa/controller/EmpresaController.java
+++ src/main/java/com/helpdesk_api/empresa/controller/EmpresaController.java
@@ -4,27 +4,27 @@ import com.helpdesk_api.empresa.dto.EmpresaRequestDto;
 import com.helpdesk_api.empresa.dto.EmpresaResponseDto;
 import com.helpdesk_api.empresa.service.EmpresaService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/empresas")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class EmpresaController {
 
     private final EmpresaService empresaService;
 
     @PostMapping
     public ResponseEntity<EmpresaResponseDto> criarEmpresa(@Valid @RequestBody EmpresaRequestDto requestDto) {
-
         EmpresaResponseDto response = empresaService.criarEmpresa(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
-
     }
 
 
