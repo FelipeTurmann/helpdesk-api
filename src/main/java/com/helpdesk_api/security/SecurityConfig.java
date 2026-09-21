107be527a66937f7156a3d3077e79ce474963909
FelipeTurmann <felipe.turman@gmail.com>
2026-09-21 19:09:07 -0300

    fix: ajusta securityConfig liberando swagger

--- src/main/java/com/helpdesk_api/security/SecurityConfig.java
+++ src/main/java/com/helpdesk_api/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
